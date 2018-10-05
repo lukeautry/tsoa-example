c4b356be3eb82268185d715164aaba7451012b51
thinhvoxuan <thinhvoxuan@gmail.com>
2018-10-05 16:45:17 +0700

    upgrade to newer lib version

--- src/routes.ts
+++ src/routes.ts
@@ -1,222 +1,219 @@
 /* tslint:disable */
-import {ValidateParam} from 'tsoa';
-import {Controller} from 'tsoa';
-import {AccountsController} from './controllers/accountsController';
-import {UsersController} from './controllers/usersController';
+import { Controller, ValidateParam, FieldErrors, ValidateError, TsoaRoute } from 'tsoa';
+import { AccountsController } from './controllers/accountsController';
+import { UsersController } from './controllers/usersController';
 
-const models: any = {
-  "User": {
-    "id": {"required": true, "typeName": "double"},
-    "email": {"required": true, "typeName": "string"},
-    "createdAt": {"required": true, "typeName": "datetime"},
-  },
-  "TestAccount": {
-    "id": {"required": true, "typeName": "double"},
-    "address": {"required": false, "typeName": "string"},
-    "name": {"required": true, "typeName": "string"},
-    "users": {"required": false, "typeName": "array", "array": {"typeName": "User"}},
-    "fields": {"required": false, "typeName": "array", "array": {"typeName": "string"}},
-  },
-  "UserCreateRequest": {
-    "email": {"required": true, "typeName": "string"},
-  },
-  "UserUpdateRequest": {
-    "createdAt": {"required": false, "typeName": "datetime"},
-    "email": {"required": true, "typeName": "string"},
-  },
+const models: TsoaRoute.Models = {
+    "User": {
+        "properties": {
+            "id": { "dataType": "double", "required": true },
+            "email": { "dataType": "string", "required": true },
+            "createdAt": { "dataType": "datetime", "required": true },
+        },
+    },
+    "TestAccount": {
+        "properties": {
+            "id": { "dataType": "double", "required": true },
+            "address": { "dataType": "string" },
+            "name": { "dataType": "string", "required": true },
+            "users": { "dataType": "array", "array": { "ref": "User" } },
+            "fields": { "dataType": "array", "array": { "dataType": "string" } },
+        },
+    },
+    "UserCreateRequest": {
+        "properties": {
+            "email": { "dataType": "string", "required": true },
+        },
+    },
+    "UserUpdateRequest": {
+        "properties": {
+            "createdAt": { "dataType": "datetime" },
+            "email": { "dataType": "string", "required": true },
+        },
+    },
 };
 
-
-/* tslint:disable:forin */
 export function RegisterRoutes(app: any) {
-  app.get('/v1/Accounts/Current',
-    function (request: any, response: any, next: any) {
-      const args = {};
+    app.get('/v1/Accounts/Current',
+        function(request: any, response: any, next: any) {
+            const args = {
+            };
 
-      let validatedArgs: any[] = [];
-      try {
-        validatedArgs = getValidatedArgs(args, request);
-      } catch (err) {
-        return next(err);
-      }
+            let validatedArgs: any[] = [];
+            try {
+                validatedArgs = getValidatedArgs(args, request);
+            } catch (err) {
+                return next(err);
+            }
 
-      const controller = new AccountsController();
-
-
-      const promise = controller.current.apply(controller, validatedArgs);
-      let statusCode = undefined;
-      if (controller instanceof Controller) {
-        statusCode = (controller as Controller).getStatus();
-      }
-      promiseHandler(promise, statusCode, response, next);
-    });
-  app.get('/v1/Accounts/Users',
-    function (request: any, response: any, next: any) {
-      const args = {};
-
-      let validatedArgs: any[] = [];
-      try {
-        validatedArgs = getValidatedArgs(args, request);
-      } catch (err) {
-        return next(err);
-      }
-
-      const controller = new AccountsController();
+            const controller = new AccountsController();
 
 
-      const promise = controller.getUsers.apply(controller, validatedArgs);
-      let statusCode = undefined;
-      if (controller instanceof Controller) {
-        statusCode = (controller as Controller).getStatus();
-      }
-      promiseHandler(promise, statusCode, response, next);
-    });
-  app.get('/v1/Users/Current',
-    function (request: any, response: any, next: any) {
-      const args = {};
+            const promise = controller.current.apply(controller, validatedArgs);
+            promiseHandler(controller, promise, response, next);
+        });
+    app.get('/v1/Accounts/Users',
+        function(request: any, response: any, next: any) {
+            const args = {
+            };
 
-      let validatedArgs: any[] = [];
-      try {
-        validatedArgs = getValidatedArgs(args, request);
-      } catch (err) {
-        return next(err);
-      }
+            let validatedArgs: any[] = [];
+            try {
+                validatedArgs = getValidatedArgs(args, request);
+            } catch (err) {
+                return next(err);
+            }
 
-      const controller = new UsersController();
+            const controller = new AccountsController();
 
 
-      const promise = controller.Current.apply(controller, validatedArgs);
-      let statusCode = undefined;
-      if (controller instanceof Controller) {
-        statusCode = (controller as Controller).getStatus();
-      }
-      promiseHandler(promise, statusCode, response, next);
-    });
-  app.get('/v1/Users/:userId',
-    function (request: any, response: any, next: any) {
-      const args = {
-        userId: {"in": "path", "name": "userId", "required": true, "typeName": "double"},
-      };
+            const promise = controller.getUsers.apply(controller, validatedArgs);
+            promiseHandler(controller, promise, response, next);
+        });
+    app.get('/v1/Users/Current',
+        function(request: any, response: any, next: any) {
+            const args = {
+            };
 
-      let validatedArgs: any[] = [];
-      try {
-        validatedArgs = getValidatedArgs(args, request);
-      } catch (err) {
-        return next(err);
-      }
+            let validatedArgs: any[] = [];
+            try {
+                validatedArgs = getValidatedArgs(args, request);
+            } catch (err) {
+                return next(err);
+            }
 
-      const controller = new UsersController();
+            const controller = new UsersController();
 
 
-      const promise = controller.Get.apply(controller, validatedArgs);
-      let statusCode = undefined;
-      if (controller instanceof Controller) {
-        statusCode = (controller as Controller).getStatus();
-      }
-      promiseHandler(promise, statusCode, response, next);
-    });
-  app.post('/v1/Users',
-    function (request: any, response: any, next: any) {
-      const args = {
-        request: {"in": "body", "name": "request", "required": true, "typeName": "UserCreateRequest"},
-      };
+            const promise = controller.Current.apply(controller, validatedArgs);
+            promiseHandler(controller, promise, response, next);
+        });
+    app.get('/v1/Users/:userId',
+        function(request: any, response: any, next: any) {
+            const args = {
+                userId: { "in": "path", "name": "userId", "required": true, "dataType": "double" },
+            };
 
-      let validatedArgs: any[] = [];
-      try {
-        validatedArgs = getValidatedArgs(args, request);
-      } catch (err) {
-        return next(err);
-      }
+            let validatedArgs: any[] = [];
+            try {
+                validatedArgs = getValidatedArgs(args, request);
+            } catch (err) {
+                return next(err);
+            }
 
-      const controller = new UsersController();
+            const controller = new UsersController();
 
 
-      const promise = controller.Create.apply(controller, validatedArgs);
-      let statusCode = undefined;
-      if (controller instanceof Controller) {
-        statusCode = (controller as Controller).getStatus();
-      }
-      promiseHandler(promise, statusCode, response, next);
-    });
-  app.delete('/v1/Users/:userId',
-    function (request: any, response: any, next: any) {
-      const args = {
-        userId: {"in": "path", "name": "userId", "required": true, "typeName": "double"},
-      };
+            const promise = controller.Get.apply(controller, validatedArgs);
+            promiseHandler(controller, promise, response, next);
+        });
+    app.post('/v1/Users',
+        function(request: any, response: any, next: any) {
+            const args = {
+                request: { "in": "body", "name": "request", "required": true, "ref": "UserCreateRequest" },
+            };
 
-      let validatedArgs: any[] = [];
-      try {
-        validatedArgs = getValidatedArgs(args, request);
-      } catch (err) {
-        return next(err);
-      }
+            let validatedArgs: any[] = [];
+            try {
+                validatedArgs = getValidatedArgs(args, request);
+            } catch (err) {
+                return next(err);
+            }
 
-      const controller = new UsersController();
+            const controller = new UsersController();
 
 
-      const promise = controller.Delete.apply(controller, validatedArgs);
-      let statusCode = undefined;
-      if (controller instanceof Controller) {
-        statusCode = (controller as Controller).getStatus();
-      }
-      promiseHandler(promise, statusCode, response, next);
-    });
-  app.patch('/v1/Users',
-    function (request: any, response: any, next: any) {
-      const args = {
-        request: {"in": "body", "name": "request", "required": true, "typeName": "UserUpdateRequest"},
-      };
+            const promise = controller.Create.apply(controller, validatedArgs);
+            promiseHandler(controller, promise, response, next);
+        });
+    app.delete('/v1/Users/:userId',
+        function(request: any, response: any, next: any) {
+            const args = {
+                userId: { "in": "path", "name": "userId", "required": true, "dataType": "double" },
+            };
 
-      let validatedArgs: any[] = [];
-      try {
-        validatedArgs = getValidatedArgs(args, request);
-      } catch (err) {
-        return next(err);
-      }
+            let validatedArgs: any[] = [];
+            try {
+                validatedArgs = getValidatedArgs(args, request);
+            } catch (err) {
+                return next(err);
+            }
 
-      const controller = new UsersController();
+            const controller = new UsersController();
 
 
-      const promise = controller.Update.apply(controller, validatedArgs);
-      let statusCode = undefined;
-      if (controller instanceof Controller) {
-        statusCode = (controller as Controller).getStatus();
-      }
-      promiseHandler(promise, statusCode, response, next);
-    });
+            const promise = controller.Delete.apply(controller, validatedArgs);
+            promiseHandler(controller, promise, response, next);
+        });
+    app.patch('/v1/Users',
+        function(request: any, response: any, next: any) {
+            const args = {
+                request: { "in": "body", "name": "request", "required": true, "ref": "UserUpdateRequest" },
+            };
+
+            let validatedArgs: any[] = [];
+            try {
+                validatedArgs = getValidatedArgs(args, request);
+            } catch (err) {
+                return next(err);
+            }
+
+            const controller = new UsersController();
 
 
-  function promiseHandler(promise: any, statusCode: any, response: any, next: any) {
-    return promise
-      .then((data: any) => {
-        if (data) {
-          response.json(data);
-          response.status(statusCode || 200);
-        } else {
-          response.status(statusCode || 204);
-          response.end();
+            const promise = controller.Update.apply(controller, validatedArgs);
+            promiseHandler(controller, promise, response, next);
+        });
+
+
+    function isController(object: any): object is Controller {
+        return 'getHeaders' in object && 'getStatus' in object && 'setStatus' in object;
+    }
+
+    function promiseHandler(controllerObj: any, promise: any, response: any, next: any) {
+        return Promise.resolve(promise)
+            .then((data: any) => {
+                let statusCode;
+                if (isController(controllerObj)) {
+                    const headers = controllerObj.getHeaders();
+                    Object.keys(headers).forEach((name: string) => {
+                        response.set(name, headers[name]);
+                    });
+
+                    statusCode = controllerObj.getStatus();
+                }
+
+                if (data || data === false) { // === false allows boolean result
+                    response.status(statusCode || 200).json(data);
+                } else {
+                    response.status(statusCode || 204).end();
+                }
+            })
+            .catch((error: any) => next(error));
+    }
+
+    function getValidatedArgs(args: any, request: any): any[] {
+        const fieldErrors: FieldErrors = {};
+        const values = Object.keys(args).map((key) => {
+            const name = args[key].name;
+            switch (args[key].in) {
+                case 'request':
+                    return request;
+                case 'query':
+                    return ValidateParam(args[key], request.query[name], models, name, fieldErrors);
+                case 'path':
+                    return ValidateParam(args[key], request.params[name], models, name, fieldErrors);
+                case 'header':
+                    return ValidateParam(args[key], request.header(name), models, name, fieldErrors);
+                case 'body':
+                    return ValidateParam(args[key], request.body, models, name, fieldErrors, name + '.');
+                case 'body-prop':
+                    return ValidateParam(args[key], request.body[name], models, name, fieldErrors, 'body.');
+            }
+        });
+        if (Object.keys(fieldErrors).length > 0) {
+            throw new ValidateError(fieldErrors, '');
         }
-      })
-      .catch((error: any) => next(error));
-  }
-
-  function getValidatedArgs(args: any, request: any): any[] {
-    return Object.keys(args).map(key => {
-      const name = args[key].name;
-      switch (args[key].in) {
-        case 'request':
-          return request;
-        case 'query':
-          return ValidateParam(args[key], request.query[name], models, name)
-        case 'path':
-          return ValidateParam(args[key], request.params[name], models, name)
-        case 'header':
-          return ValidateParam(args[key], request.header(name), models, name);
-        case 'body':
-          return ValidateParam(args[key], request.body, models, name);
-        case 'body-prop':
-          return ValidateParam(args[key], request.body[name], models, name);
-      }
-    });
-  }
+        return values;
+    }
 }
--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,12 @@ import * as methodOverride from 'method-override';
 import { RegisterRoutes } from './routes';
 
 const app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+  
 
 app.use('/docs', express.static(__dirname + '/swagger-ui'));
 app.use('/swagger.json', (req, res) => {
